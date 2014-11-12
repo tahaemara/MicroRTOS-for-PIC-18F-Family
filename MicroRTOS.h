2178519af038f968a33a0af5bc26bb607a5b8f22
Taha Emara <tahaemarah@yahoo.com>
2014-11-13 00:29:19 +0200

    Create MicroRTOS.h

---
+++ MicroRTOS.h
@@ -0,0 +1,9 @@
+#ifndef _MICRORTOS_h  /* Include guard */
+#define _MICRORTOS_h
+////
+#pragma disablecontextsaving  // instruct the compiler not to automatically perform context-switching 
+                             //  and let me do that via ISR.
+                             
+#define numOfTasks 3         //  number of tasks
+/////
+#endif
